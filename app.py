2b2c09fb92c1fda363dd0cb3274ff96786b74124
Rory Hartong-Redden <rory@gmail.com>
2018-01-31 19:24:56 -0800

    Local demo works

--- app.py
+++ app.py
@@ -8,12 +8,18 @@
 # https://stackoverflow.com/questions/20107414/passing-a-matplotlib-figure-to-html-flask
 
 import pandas as pd
+import matplotlib as mpl
+mpl.use('agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import io
 from flask import Flask, make_response, send_file, render_template
 
 app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 def generate_sample_plot():
     fig = plt.figure()
@@ -29,18 +35,21 @@ def figure_to_bytes(figure):
     image_bytes.seek(0)
     return image_bytes
 
-@app.route('/images/<cropzonekey>')
-def images(cropzonekey):
-    return render_template("images.html", title=cropzonekey)
+@app.route('/activities/<activity_id>')
+def images(activity_id):
+    return render_template("images.html", title=activity_id)
 
-@app.route('/fig/<cropzonekey>')
-def fig(cropzonekey):
+@app.route('/fig/<activity_id>')
+def fig(activity_id):
     fig = generate_sample_plot()
     return send_file(figure_to_bytes(fig), mimetype='image/png')
 
 @app.route('/plot3')
 def plot3():
-    fig = generate_sample_plot()
+    fig = plt.figure()
+    ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
+    ts = ts.cumsum()
+    ax_plot = ts.hist(ax=plt.gca())
     return send_file(figure_to_bytes(fig), mimetype='image/png')
 
 
